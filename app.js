d80c270ee9eb4a003a83c321e00bbfde57ffc65c
Jameson Farmer <thejamesonfarmer@gmail.com>
2025-11-04 10:52:51 -0800

    added sample test

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const app = express();
 
 app.get('/', (req, res) => res.status(200).json({ hello: 'hello world' }));
 
-
 app.listen(PORT, () => {
   console.log(`Running on http://localhost:${PORT}`);
 });
+
+
+export default function sum(a, b) {
+  return a + b;
+}
