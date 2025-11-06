0b951f88e545a598013d6d9760424a78f87b497a
Madeleine Chance <mechance782@gmail.com>
2025-11-06 10:29:53 -0800

    add comments

--- db.js
+++ db.js
@@ -8,19 +8,27 @@ if (process.env.NODE_ENV === 'test') {
   const sqlite = new Database(':memory:');
 
   // Example schema setup
+  // Consider adding .exec method to db wrapper
   sqlite.exec(`
     CREATE TABLE users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT
     );
   `);
-
+  // wrapper db object to mimic mysql2 behavior using sqlite
   db = {
+    // methods are async only to match the mysql2 method behavior
     async query(sql, params = []) {
+      // turn sql string into sqlite Statement object
       const stmt = sqlite.prepare(sql);
+
+      // check what kind of query it is to determine expected output
       if (sql.trim().toLowerCase().startsWith('select')) {
+        // retrieves all rows matching the query as an array
         return [stmt.all(params)];
       } else {
+        // If it's not selecting/reading then its changing data.
+        // this method runs the statement and returns the number of rows changed, and the last row inserted.
         const info = stmt.run(params);
         return [{ insertId: info.lastInsertRowid, changes: info.changes }];
       }
